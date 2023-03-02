cf747fd0b7d59d483b58d32b301baf8739aceb44
XxDevilsCloverxX <silasrodriguez02@gmail.com>
2023-03-01 21:09:20 -0600

    PlateReader Update

--- Plate_Scanner/plate_scan.py
+++ Plate_Scanner/plate_scan.py
@@ -6,6 +6,7 @@ import cv2
 from pandas import read_csv
 import os
 from detecto.core import Model
+import pymysql
 
 """
 @Author: xxdevilscloverxx
@@ -19,23 +20,30 @@ from detecto.core import Model
 """
 class plate_reader:
     
-    #initialize the reader
-    def __init__(self, lang='en', gpu=True, filter=None, model_path=None):
+    #initialize the reader, database, and model
+    def __init__(self, lang='en', gpu=True, filter=None, model_path=None, host='localhost', user='root', password='password', db='database'):
         self.reader = ocr.Reader([lang], gpu=gpu)
         self.filter = filter
         self.time = datetime.now()  #get the current time @ initialization
         self.buffer = {}  #initialize the buffer
         self.time_buffer = {}  #initialize the frame buffer -> prevent duplicate uploads
-        #load the model
-        if model_path is not None:
-            self.platemodel = Model.load(file=model_path, classes=['licence'])
-        else:
-            raise Exception('Model path not provided!')
+        try:
+            self.connection = pymysql.connect(host=host, user=user, password=password, db=db)  #connect to the database
+            self.connected = True
+        except:
+            self.connected = False
+            print('Database not found! Please check the database credentials and try again! Printing results to console instead...')
+        finally:
+            #load the model
+            if model_path is not None:
+                self.platemodel = Model.load(file=model_path, classes=['licence'])
+            else:
+                raise Exception('Model path not provided!')
     
     #define a function to read the plate from an image
     def read_plate(self, plate_img):
         #read the plate from image
-        results = self.reader.readtext(plate_img, detail=0)
+        results = self.reader.readtext(plate_img, detail=0, text_threshold=.9)  #play with threshold to get better results
         #apply a filter to the results to remove unwanted strings and states, if a filter is provided
         if self.filter is not None:
             #filter the results for the plate + remove useless strings and states
@@ -52,14 +60,23 @@ class plate_reader:
         plate_text = [char for char in results[0] if char.isalnum()]
         plate_text = "".join(plate_text)
         plate_text = plate_text.upper()
+
+        #check if the plate is valid
+        if len(plate_text) < 5 or len(plate_text) > 8 or plate_text.isdigit():
+            return ""
+        
         return plate_text
     
     #define a function to read the plate from a video
-    def process_video(self, video_path=0):
+    def process_video(self, video_path=0, frame_width=640, frame_height=480, fps=30):
         #initialize the video reader
         cap = cv2.VideoCapture(video_path)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+        cap.set(cv2.CAP_PROP_FPS, fps)
+
         #wait for the camera to initialize
-        time.sleep(2)
+        time.sleep(5)
         #loop over the frames
         while True:
             #read the frame
@@ -99,7 +116,10 @@ class plate_reader:
             cv2.imshow('Frame', frame)
             #exit on 'q'
             if cv2.waitKey(1) & 0xFF == ord('q'):
+                if self.connected:
+                    self.connection.close()
                 break
+        
         #release the camera
         cap.release()
         cv2.destroyAllWindows()
@@ -137,33 +157,53 @@ class plate_reader:
     def upload_results(self):
         #check if 5 seconds have passed
         if datetime.now() - self.time > timedelta(seconds=10):
-            i = 0
-            print("Uploading results to database...")
-            #reset the timer
-            self.time = datetime.now()
-            #loop over the buffer
-            for plate in self.buffer.keys():
-                if plate not in self.time_buffer.keys():
-                    i += 1
-                    #upload the results to the database
-                    print(f"{plate}: {self.buffer[plate]} uploaded to database!")
-            
+            i = 0  #initialize the counter
+            self.time = datetime.now()   #reset the timer
+            if self.connected:
+                print("Uploading results to database...")
+                #loop over the buffer
+                for plate in self.buffer.keys():
+                    if plate not in self.time_buffer.keys():
+                        with self.connection.cursor() as cursor:
+                            #create the sql query
+                            sql = "INSERT INTO `plates` (`Plate`, `Time`) VALUES (%s, %s)"
+                            #execute the query
+                            cursor.execute(sql, (plate, self.buffer[plate]))
+                            #commit the changes
+                            self.connection.commit()
+                        i += 1
+                        
+                        #upload the results to the database
+                        print(f"{plate}: {self.buffer[plate]} uploaded to database!")
+            else:
+                print("Database connection failed! Printing results...")
+                #loop over the buffer
+                for plate in self.buffer.keys():
+                    if plate not in self.time_buffer.keys():
+                        print(f"{plate}: {self.buffer[plate]}")
+                        i += 1
             print(f"{i} results uploaded to database!")
-            
-            #update the time buffer
-            self.time_buffer.clear()    #clear the time buffer before updating
-            self.time_buffer.update(self.buffer)
 
-            print(self.time_buffer)    #print the time buffer -> show plate when empty!
+            #update the time buffer
+            self.time_buffer.clear()              #clear the time buffer before updating
+            self.time_buffer.update(self.buffer)  #update the time buffer
+
+            print(self.time_buffer)               #print the time buffer -> show plate when empty!
 
             #clear the buffer
             self.buffer.clear()
-            print('Upload complete!')
+            print('Upload completed!')
         return None
-        
 
 #run test script
 if __name__ == '__main__':
+
+    #define the sql connection
+    host = 'localhost'
+    user = None
+    password = None
+    db = None
+
     model_path = '/home/xxdevilscloverxx/Documents/models/plates.pth'
     # define a filter path
     filter_path = os.path.join(os.path.dirname(__file__), 'states.csv')
@@ -172,6 +212,7 @@ if __name__ == '__main__':
     filter = filter['State'].tolist()
 
     #initialize the reader
-    detector = plate_reader(filter=filter, model_path=model_path)
+    detector = plate_reader(filter=filter, model_path=model_path,
+                             gpu=True, host=host, user=user, password=password, db=db)
     #process the video
     detector.process_video()
