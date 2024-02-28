9106d5cd477632f707476b6685a729de23120de6
xVoidDevilx <silasrodriguez02@gmail.com>
2024-02-28 13:14:05 -0600

    Added timing

--- ML_PR/Pattern Rec/Project 2/Rodriguez_Silas_Project2.py
+++ ML_PR/Pattern Rec/Project 2/Rodriguez_Silas_Project2.py
@@ -30,7 +30,7 @@ def SVM_Evaluator(weights: np.ndarray, X: np.ndarray, t: np.ndarray):
     accuracy = 1 - misclasses / t.shape[0]
     return misclasses, index_misclasses[0], accuracy*100
 
-def SoftMargin_SVM(X:np.ndarray, t:np.ndarray, C:float, req_reclass:bool=False):
+def SoftMargin_SVM(X:np.ndarray, t:np.ndarray, C:float, req_reclass:bool=False, plot_en:bool=True):
     """
     @purpose:
         - Calcaulte the Soft margin SVM using the dual form solution
@@ -73,6 +73,7 @@ def SoftMargin_SVM(X:np.ndarray, t:np.ndarray, C:float, req_reclass:bool=False):
     b_cvxopt = matrix(b, tc='d')
 
     # # Solve the quadratic programming problem
+    solvers.options['show_progress'] = False
     sol = solvers.qp(P=P_cvxopt, q=q_cvxopt, G=G_cvxopt, h=h_cvxopt, A=A_cvxopt, b=b_cvxopt)
 
     # Extract the optimal solution
@@ -84,19 +85,13 @@ def SoftMargin_SVM(X:np.ndarray, t:np.ndarray, C:float, req_reclass:bool=False):
     # Selecting the set of indices S corresponding to non zero parameters
     S = (lambdas > 1e-4).flatten()
 
-    # Computing b and append to weights - investigate the failure here
-    b = y[S] - np.dot(dual_X[S], weights)
+    # Computing b and append to weights
+    b = y[S] - dual_X[S] @ weights
     weights = np.vstack((weights, b.mean()))
     time_end = time()
 
     # identify the support vectors
     support_vectors = dual_X[S]
-    
-    # Display results
-    print("Optimal solution w:")
-    print(weights)
-    print("Lambdas (dual):")
-    print(lambdas[S])
 
     # plot the decision boundary and margins
     x_line = np.linspace(X[:, 0].min(), X[:, 0].max(), 100) # create a 100 evenly spaced points over x1
@@ -108,42 +103,49 @@ def SoftMargin_SVM(X:np.ndarray, t:np.ndarray, C:float, req_reclass:bool=False):
     vert_offset = np.sqrt(1 + slope**2) * d   # proof here: https://www.geeksforgeeks.org/distance-between-two-lines/ x2 =y x1 =x for slope
     margin1 = y_line + vert_offset
     margin2 = y_line - vert_offset
-    print(f'Margin width: 2/||w|| = {2 * d}')
 
     # Test classifications:
     dual_X = np.c_[X, np.ones(X.shape[0])]
     misclasses, index_misclasses, accuracy = SVM_Evaluator(weights=weights, X=dual_X, t=y)
-    print(f'{misclasses} misclasses with {accuracy}% accuracy!')
+    
+    # Display results
+    if plot_en:
+        print("Optimal solution w:")
+        print(weights)
+        print("Lambdas (dual):")
+        print(lambdas[S])
+        print(f'Margin width: 2/||w|| = {2 * d}')
+        print(f'{misclasses} misclasses with {accuracy}% accuracy!')
 
     # Plotting misclassified points with black squares
     misclassified_points = X[index_misclasses]  # Extract misclassified points
 
+    if plot_en:
+        plt.figure(figsize=(8,6))
+        plt.title(f'Data with d(x) + Margins: C={C}')
+        plt.scatter(support_vectors[:, 0], support_vectors[:, 1], facecolors='none', edgecolors='k', s=150, label=f'Support Vectors: {np.sum(S)}')    # highlight SVs
+        plt.scatter(misclassified_points[:, 0], misclassified_points[:, 1], marker='s', edgecolors='black', facecolors='none', s=150, label=f'Misclassified Vectors: {misclasses}')
+        plt.scatter(X[:,0], X[:,1], c=t, cmap='coolwarm')
+        plt.plot(x_line, y_line, c='black', label='d(x) = 0')
+        plt.plot(x_line, margin1, c='red', label='d(x) = 1')
+        plt.plot(x_line, margin2, c='blue', label='d(x) = -1')
 
-    plt.figure(figsize=(8,6))
-    plt.title(f'Data with d(x) + Margins: C={C}')
-    plt.scatter(support_vectors[:, 0], support_vectors[:, 1], facecolors='none', edgecolors='k', s=150, label=f'Support Vectors: {np.sum(S)}')    # highlight SVs
-    plt.scatter(misclassified_points[:, 0], misclassified_points[:, 1], marker='s', edgecolors='black', facecolors='none', s=150, label=f'Misclassified Vectors: {misclasses}')
-    plt.scatter(X[:,0], X[:,1], c=t, cmap='coolwarm')
-    plt.plot(x_line, y_line, c='black', label='d(x) = 0')
-    plt.plot(x_line, margin1, c='red', label='d(x) = 1')
-    plt.plot(x_line, margin2, c='blue', label='d(x) = -1')
+        # Shade the entire region below margin2 in blue
+        plt.fill_between(x_line, y_line, min(margin2), color='blue', alpha=0.2, label='Positive Region (C0)')
+        # Shade the entire region above margin1 in red
+        plt.fill_between(x_line, y_line, max(margin1), color='red', alpha=0.2, label='Negative Region (C1)')
+        # shade the margin with gray
+        plt.fill_between(x_line, margin1, margin2, color='black', alpha=0.2, label='Maximized Margin')
 
-    # Shade the entire region below margin2 in blue
-    plt.fill_between(x_line, y_line, min(margin2), color='blue', alpha=0.2, label='Positive Region (C0)')
-    # Shade the entire region above margin1 in red
-    plt.fill_between(x_line, y_line, max(margin1), color='red', alpha=0.2, label='Negative Region (C1)')
-    # shade the margin with gray
-    plt.fill_between(x_line, margin1, margin2, color='black', alpha=0.2, label='Maximized Margin')
-
-    plt.legend()
-    plt.xlabel('x1')
-    plt.ylabel('x2')
-    plt.colorbar(label='Classes')
-    plt.show()
+        plt.legend()
+        plt.xlabel('x1')
+        plt.ylabel('x2')
+        plt.colorbar(label='Classes')
+        plt.show()
 
     return weights, time_end-time_start
 
-def SkLearn_SVM(X:np.ndarray, t:np.ndarray, C:float, req_reclass:bool=False):
+def SkLearn_SVM(X:np.ndarray, t:np.ndarray, C:float, req_reclass:bool=False, plot_en:bool=True):
     """
     @purpose:
         - Calcaulte the Soft margin SVM using the sklearn solution
@@ -162,13 +164,6 @@ def SkLearn_SVM(X:np.ndarray, t:np.ndarray, C:float, req_reclass:bool=False):
     clf.fit(sk_x, sk_y.ravel()) 
     time_end = time()
 
-    print('w = ',clf.coef_)
-    print('b = ',clf.intercept_)
-    print('Indices of support vectors = ', clf.support_)
-    print('Support vectors = ', clf.support_vectors_)
-    print('Number of support vectors for each class = ', clf.n_support_)
-    print('Coefficients of the support vector in the decision function = ', np.abs(clf.dual_coef_))
-
     # get the support vectors
     support_vectors = sk_x[clf.support_]
     # get the weights
@@ -186,40 +181,72 @@ def SkLearn_SVM(X:np.ndarray, t:np.ndarray, C:float, req_reclass:bool=False):
     vert_offset = np.sqrt(1 + slope**2) * d   # proof here: https://www.geeksforgeeks.org/distance-between-two-lines/ x2 =y x1 =x for slope
     margin1 = y_line + vert_offset
     margin2 = y_line - vert_offset
-    print(f'Margin width: 2/||w|| = {2 * d}')
 
     # Test classifications:
     sk_x = np.c_[X, np.ones(X.shape[0])]
     misclasses, index_misclasses, accuracy = SVM_Evaluator(weights=weights, X=sk_x, t=sk_y)
-    print(f'{misclasses} misclasses with {accuracy}% accuracy!')
+
+    if plot_en:
+        print('w = ',clf.coef_)
+        print('b = ',clf.intercept_)
+        print('Indices of support vectors = ', clf.support_)
+        print('Support vectors = ', clf.support_vectors_)
+        print('Number of support vectors for each class = ', clf.n_support_)
+        print('Coefficients of the support vector in the decision function = ', np.abs(clf.dual_coef_))
+        print(f'Margin width: 2/||w|| = {2 * d}')
+        print(f'{misclasses} misclasses with {accuracy}% accuracy!')
 
     # Plotting misclassified points with black squares
     misclassified_points = X[index_misclasses]  # Extract misclassified points
 
-    plt.figure(figsize=(8,6))
-    plt.title(f'Data with d(x) + Margins - SkLearn C={C}')
-    plt.scatter(support_vectors[:, 0], support_vectors[:, 1], facecolors='none', edgecolors='k', s=150, label=f'Support Vectors: {len(support_vectors)}')    # highlight SVs
-    plt.scatter(misclassified_points[:, 0], misclassified_points[:, 1], marker='s', edgecolors='black', facecolors='none', s=150, label=f'Misclassified Vectors: {misclasses}')
-    plt.scatter(X[:,0], X[:,1], c=t, cmap='coolwarm')
-    plt.plot(x_line, y_line, c='black', label='d(x) = 0')
-    plt.plot(x_line, margin1, c='red', label='d(x) = 1')
-    plt.plot(x_line, margin2, c='blue', label='d(x) = -1')
+    if plot_en:
+        plt.figure(figsize=(8,6))
+        plt.title(f'Data with d(x) + Margins - SkLearn C={C}')
+        plt.scatter(support_vectors[:, 0], support_vectors[:, 1], facecolors='none', edgecolors='k', s=150, label=f'Support Vectors: {len(support_vectors)}')    # highlight SVs
+        plt.scatter(misclassified_points[:, 0], misclassified_points[:, 1], marker='s', edgecolors='black', facecolors='none', s=150, label=f'Misclassified Vectors: {misclasses}')
+        plt.scatter(X[:,0], X[:,1], c=t, cmap='coolwarm')
+        plt.plot(x_line, y_line, c='black', label='d(x) = 0')
+        plt.plot(x_line, margin1, c='red', label='d(x) = 1')
+        plt.plot(x_line, margin2, c='blue', label='d(x) = -1')
 
-    # Shade the entire region below margin2 in blue
-    plt.fill_between(x_line, y_line, min(margin2), color='blue', alpha=0.2, label='Positive Region (C0)')
-    # Shade the entire region above margin1 in red
-    plt.fill_between(x_line, y_line, max(margin1), color='red', alpha=0.2, label='Negative Region (C1)')
-    # shade the margin with gray
-    plt.fill_between(x_line, margin1, margin2, color='black', alpha=0.2, label='Margin')
+        # Shade the entire region below margin2 in blue
+        plt.fill_between(x_line, y_line, min(margin2), color='blue', alpha=0.2, label='Positive Region (C0)')
+        # Shade the entire region above margin1 in red
+        plt.fill_between(x_line, y_line, max(margin1), color='red', alpha=0.2, label='Negative Region (C1)')
+        # shade the margin with gray
+        plt.fill_between(x_line, margin1, margin2, color='black', alpha=0.2, label='Margin')
 
-    plt.legend()
-    plt.xlabel('x1')
-    plt.ylabel('x2')
-    plt.colorbar(label='Classes')
-    plt.show()
+        plt.legend()
+        plt.xlabel('x1')
+        plt.ylabel('x2')
+        plt.colorbar(label='Classes')
+        plt.show()
 
 
     return weights, time_end-time_start
+
+def generate_linearly_nonseparable_data(N:int):
+    """
+    @purpose:
+        Generate linearly separable data easily with labels
+    @param:
+        N - Number of data points per class
+    @return:
+        X - data points
+        y - labels
+    """
+    class1_x = np.random.normal(loc=25, size=(N,2))
+    class2_x = np.random.normal(loc=28.5, size=(N,2))
+
+    class1_t = np.zeros((N, 1))
+    class2_t = np.ones((N, 1))
+
+    dataset = np.vstack((np.hstack((class1_x, class1_t)), np.hstack((class2_x, class2_t))))
+    np.random.shuffle(dataset)
+
+    X = dataset[:, :-1]
+    y = dataset[:, -1].reshape(-1, 1)
+    return X, y
 
 def main():
 
@@ -239,40 +266,46 @@ def main():
     plt.colorbar(label='Classes')
     plt.show()
     #########################################################################
-    times_dual  = []
-    times_sklrn = []
-    #########################################################################
     # soft margin SVM with C = 0.1
-    _, elapsed_time = SoftMargin_SVM(X = X, t=t, C=0.1)
-    times_dual.append(elapsed_time)
+    SoftMargin_SVM(X = X, t=t, C=0.1)
     #########################################################################
     # soft margin SVM with C = 100
-    _, elapsed_time = SoftMargin_SVM(X = X, t=t, C=100)
-    times_dual.append(elapsed_time)
+    SoftMargin_SVM(X = X, t=t, C=100)
+    
     #########################################################################
     # sklearn solution for C = 0.1
-    _, elapsed_time = SkLearn_SVM(X = X, t= t, C=0.1)
-    times_sklrn.append(elapsed_time)
+    SkLearn_SVM(X = X, t= t, C=0.1)
+    
     #########################################################################
     # sklearn solution for C = 100
-    _, elapsed_time = SkLearn_SVM(X = X, t= t, C=100)
-    times_sklrn.append(elapsed_time)
-
-    #########################################################################
-    # get the average run times
-    avg_dual  = sum(times_dual)  / len(times_dual)
-    avg_sklrn = sum(times_sklrn) / len(times_sklrn)
-
-    print()
-    print(f'Average runtime for implemented dual form solution - {avg_dual}')
-    print(f'Average runtime for sklearn solution - {avg_sklrn}')
+    SkLearn_SVM(X = X, t= t, C=100)
     
-    # Assuming avg_dual and avg_sklrn are the average runtimes for the implemented dual form and sklearn solution respectively
-    percentage_difference = ((avg_dual - avg_sklrn) / avg_sklrn) * 100
-    if avg_dual > avg_sklrn:
-        print(f'The implemented dual form had {percentage_difference:.2f}% more runtime than sklearn.')
-    else:
-        print(f'The implemented dual form had {abs(percentage_difference):.2f}% less runtime than sklearn.')
+    #########################################################################
+    # Time differences
+    times_dual  = []
+    times_sklrn = []
+    x_plot = [i for i in range(50, 501, 50)]
+
+    np.random.seed(0)
+    for samples in x_plot:
+        print(f'Num Samples working : {samples}')
+        X, t = generate_linearly_nonseparable_data(N=samples)
+        _, time_dual = SoftMargin_SVM(X=X, t=t, C=10, req_reclass=True, plot_en=False)
+        _, time_sklr = SkLearn_SVM(X=X, t=t, C=10, req_reclass=True, plot_en=False)
+
+        times_dual.append(time_dual)
+        times_sklrn.append(time_sklr)
+    
+    print('Plotting...')
+    plt.figure(figsize=(8,6))
+    plt.plot(x_plot, times_dual, c='red', label='CVXOPT time')
+    plt.plot(x_plot, times_sklrn, c='blue', label='LIBCVM time')
+    plt.xlabel('N Samples')
+    plt.ylabel('Elapsed Time (s)')
+    plt.title('LIBSVM vs CVXOPT Computational Time for Weights vs # Samples')
+    plt.legend()
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
